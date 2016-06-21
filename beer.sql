1319abf496b9e4c4dd95184c4afdfa7234abace5
ilanitiz <izrailovi@live.bucks.edu>
2016-06-20 22:19:26 -0400

    Add files via upload

---
+++ beer.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: beer
+-- ------------------------------------------------------
+-- Server version	5.7.12-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `beer`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `beer` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `beer`;
+
+--
+-- Table structure for table `beer`
+--
+
+DROP TABLE IF EXISTS `beer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `beer` (
+  `beerID` int(11) NOT NULL AUTO_INCREMENT,
+  `beerName` varchar(25) DEFAULT NULL,
+  `beerStyle` varchar(25) DEFAULT NULL,
+  `beerType` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`beerID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `beer`
+--
+
+LOCK TABLES `beer` WRITE;
+/*!40000 ALTER TABLE `beer` DISABLE KEYS */;
+INSERT INTO `beer` VALUES (1,'Leffe Blonde','Ale','Blonde'),(2,'Samuel Adams Boston Lager','Lager','Amber'),(3,'Wellington Iron Duke','Malt','Dark');
+/*!40000 ALTER TABLE `beer` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-06-06 18:03:18
